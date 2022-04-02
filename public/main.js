b01e2cc405b322223e47972130179d7f7bdff6df
Jem Herrera <jhneramuz@gmail.com>
2022-04-01 19:22:10 -0600

    .

---
+++ public/main.js
@@ -0,0 +1,10 @@
+// COMPLETED
+// Research on how to host locally using node
+// install gulp and setup scss
+
+// TO DO:
+// create wireframe on figma. Use helcim brand colors and font
+// build iphone se version first
+// use social media as references for ui/ux
+// scale up to tablet. Desktop will be almost same as tablet
+// test on real device. Figure out how to run the files on a mobile device
