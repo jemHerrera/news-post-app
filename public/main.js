9337848a552626bee1a86480d388c340506c2402
Jem Herrera <jhneramuz@gmail.com>
2022-04-03 13:04:02 -0600

    .

--- public/main.js
+++ public/main.js
@@ -1,24 +1,37 @@
-// [posts] is an array of objects where post data is stored. 
-// Every post is an object pushed into [posts]
-// post object contains string and array of images
-var posts = []
 
-// toggle() to show and hide elements
-// 1st parameter: target element
+// where uploaded image src are stored before post
+var stagedImages = [];
+
+// show and hide modals
 // 2nd parameter: boolean. Add 'displayed' class if true; remove if false
 function toggleElement(element, showElement){
-    if(showElement) document.querySelector(element).classList.add('displayed');
-    else document.querySelector(element).classList.remove('displayed')
+    if(showElement) {
+        document.querySelector(element).classList.add('displayed');
+        // prevent body from scrolling behind modal
+        document.querySelector('body').classList.add('modal-open');
+    }
+    else {
+        document.querySelector(element).classList.remove('displayed');
+        document.querySelector('body').classList.remove('modal-open');
+    }
 }
 
-// where data from user input is stored before publishing
-// gets cleared whenever posted
-var newPost = {
-    text: '',
-    images: []
+//callback function onchange of text area or image upload
+//disable post button if no user input is detected
+function checkInput(){
+    if(document.querySelector('#text').value
+    || stagedImages.length > 0) document.querySelector('#post').classList.remove('disabled');
+    else document.querySelector('#post').classList.add('disabled');
 }
 
-//Whenever image is uploaded, add image data to newPost.images, as well as create a preview element
+//For textarea: Automatically adjust height as user types more content
+function autoHeight(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight)+"px";
+}
+
+// Event listener whenever an image is uploaded. Accepts multiple image upload
+// Add image data to stagedImages & create a preview element
 document.querySelector("#images-upload").addEventListener("change", function(event){
 
     Object.values(event.target.files).forEach(file => {
@@ -32,29 +45,108 @@ document.querySelector("#images-upload").addEventListener("change", function(eve
             <img class='thumbnail' src='${event.target.result}'/>
             <span onclick="removeImage('${event.target.result}', event)" class="material-icons remove">close</span>
             `;
-            newPost.images.push(event.target.result)
+            stagedImages.push(event.target.result)
             // append element to the images-preview
-            document.querySelector("#images-preview").appendChild(li);            
-        
+            document.querySelector("#images-preview").appendChild(li);  
+            // check if image has been added to staged images. if yes, enable post button   
+            checkInput();         
         });
         
         reader.readAsDataURL(file);
     })                      
-    
 });
 
-// remove an image from newPost and delete li element
+// remove the image from stagedImages and delete upload preview
+// called when user clicks on 'x' button from the upload preview
 function removeImage(imageData, event){
-    newPost.images = newPost.images.filter(image => image != imageData);
+    stagedImages = stagedImages.filter(image => image != imageData);
     event.target.parentElement.remove();
+    document.querySelector("#images-upload").value='';
+    checkInput();
+}
+//create post element on feed
+function post(){
+    // create all the nested elements
+    let postContainer = document.createElement("div"),
+        profile = document.createElement('img'),
+        postDetails = document.createElement('div'),
+        postHeader = document.createElement('div'),
+        imagesContainer = document.createElement('div'),
+        username = document.createElement('p'),
+        handle = document.createElement('p'),
+        timestamp = document.createElement('p'),
+        postText = document.createElement('p');
+
+    // add attributes and values
+    postContainer.className = "post";
+    profile.className = "profile";
+    profile.src = 'icon.png';
+    postDetails.className = "post-details";
+    postHeader.className = "post-header";
+    imagesContainer.className = "image-container"
+    //if there's only one image uploaded, add 'single' class. This displays the image in full size
+    if(stagedImages.length == 1)imagesContainer.classList.add('single');
+    username.className = 'username';
+    username.innerText = 'Me'; //static value
+    handle.className = 'handle';
+    handle.innerText = '@myself'; //static value
+    timestamp.className = 'timestamp';
+    postText.className = 'post-text';
+    postText.innerText = document.querySelector('#text').value;
+    
+    // date
+    let today = new Date(),
+        months = ["January","February","March","April","May","June","July","August","September","October","November","December"],
+        currentTime = `${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`;
+    
+    timestamp.innerText = currentTime;
+    
+    //append elements
+    postContainer.append(profile, postDetails);
+    postDetails.append(postHeader, postText, imagesContainer);
+    postHeader.append(username, handle, timestamp);
+
+    //add uploaded images to imagesContainer
+    //include onclick parameters for image-view, such as image src, time and post text
+    stagedImages.forEach(image => {
+        var imageItem = document.createElement('img');
+        imageItem.src = image;
+        imageItem.setAttribute('onclick', `previewImage({
+            'previewImage': '${image}',
+            'text': '${document.querySelector('#text').value}',
+            'time': '${currentTime}'
+        })`)
+        imagesContainer.appendChild(imageItem);
+    })
+    document.querySelector('.feed-container').appendChild(postContainer);
+
+    //hide intro
+    if(document.querySelector('.intro')) document.querySelector('.intro').remove();
+
+    //clear new-post modal
+    resetInput();
 }
 
+//clear all inputs, hide new-post modal and empty stagedImages variable
+function resetInput(){
+    toggleElement('.new-post', false);
+    document.querySelector('#text').value = '';
+    document.querySelector('#images-upload').value = '';
+    stagedImages = [];
+    checkInput();
+    //reset textarea height
+    autoHeight(document.querySelector('#text'));
 
-//create onclick function from submit button that will:
-//1. take data from elements inside of image-view as well ass the text area and post to [posts]
-//2. close new-post modal
-//3. clear {newPost}
+    let imagesPreview = document.querySelector('#images-preview');
+    while (imagesPreview.firstChild) {
+        imagesPreview.removeChild(imagesPreview.firstChild);
+    }
+}
 
-//add cancel button as well
-// css on images, create a slider
-//css on upload button; change to image icon and remove the preview text
+//when user clicks on an image from the feed, display image view modal
+function previewImage(object){
+    toggleElement('.image-view', true);
+    document.querySelector('.preview-image').src = object.previewImage;
+    document.querySelector('.preview-timestamp').innerText = object.time;
+    document.querySelector('.preview-text').innerText = object.text;
+}
